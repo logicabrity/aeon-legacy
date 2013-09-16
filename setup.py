9c09576b4caadc9f6ef195cb45e42dda83f958cb
Marc-Antonio Bisotti <mail@marc-antonio.de>
2013-09-16 16:49:45 +0100

    add setup py
    following Hitchhiker's Guide to Packaging 1.0

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Aeon',
+    version='1.0',
+    packages=['aeon', ],
+    license='The MIT License (MIT)',
+    long_description=open('README.txt').read(),
+)
