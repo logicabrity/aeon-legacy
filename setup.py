6bd9de86f5294e4a88989e1c4888d9cff3077baf
Marc-Antonio Bisotti <mail@marc-antonio.de>
2013-09-17 01:10:17 +0100

    fixed warnings of python setup.py sdist

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from distutils.core import setup
 
 setup(
     name='Aeon',
+    url='https://github.com/sMAshdot/aeon',
+    author='Marc-Antonio Bisotti',
+    author_email='mail@marc-antonio.de',
     version='1.0',
     packages=['aeon', ],
     license='The MIT License (MIT)',
-    long_description=open('README.txt').read(),
+    long_description=open('README').read(),
 )
