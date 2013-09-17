fb4e70b57fa0eb8497487929304f7c03f30a5874
Marc-Antonio Bisotti <mail@marc-antonio.de>
2013-09-17 01:58:00 +0100

    semantic versioning in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     url='https://github.com/sMAshdot/aeon',
     author='Marc-Antonio Bisotti',
     author_email='mail@marc-antonio.de',
-    version='1.0',
+    version='1.0.0',
     packages=['aeon', ],
     license='The MIT License (MIT)',
     description=('Runtime and number of calls for designated functions, '
